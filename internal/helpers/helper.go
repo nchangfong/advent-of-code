08e5d9d7bc054d06458ac47e53212f6a7fa7a1a8
Nick Chang-Fong <nchangfong@gmail.com>
2021-12-19 12:05:34 -0500

    generics when?

--- internal/helpers/helper.go
+++ internal/helpers/helper.go
@@ -39,33 +39,3 @@ func ReadInts(filename string) ([]int, error) {
 	}
 	return lines, nil
 }
-
-type IntStack struct {
-	data []int
-}
-
-func (s *IntStack) IsEmpty() bool {
-	return len(s.data) == 0
-}
-
-func (s *IntStack) Push(i int) {
-	s.data = append(s.data, i)
-}
-
-func (s *IntStack) Pop() (int, bool) {
-	if s.IsEmpty() {
-		return 0, false
-	}
-
-	idx := len(s.data) - 1
-	res := s.data[idx]
-	s.data = s.data[:idx]
-	return res, true
-}
-
-func (s *IntStack) Peek() (int, bool) {
-	if s.IsEmpty() {
-		return 0, false
-	}
-	return s.data[len(s.data)-1], true
-}
